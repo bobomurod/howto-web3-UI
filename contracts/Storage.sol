95da6abb381cb66b27808f39fa7b15b2fa58f529
bobomurod <bobomurod.boboev@yandex.ru>
2018-04-02 08:56:25 +0300

    storage

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,13 +1,10 @@
 //0x90357874ef739c31740cc812bc9be07c7bc4a52b contract address on rinkeby
 
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
 
 contract Storage{
     uint number;
     function set(uint _number){
         number = _number;
-    }
-    function get() public view returns(uint){
-        return number;
     }
 }
