eaac5af2c1d657a5b33b84b3efcdb9ac62080fcc
bobomurod <bobomurod.boboev@yandex.ru>
2018-04-02 08:59:30 +0300

    added events

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,10 +1,16 @@
-//0x90357874ef739c31740cc812bc9be07c7bc4a52b contract address on rinkeby
+// contract address 0x2309206CC02A25B2749C918CCa1B0258A9bB4CEF
 
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.19;
 
 contract Storage{
     uint number;
+    event Numbers(uint number);
     function set(uint _number){
         number = _number;
+        Numbers(number);
+    }
+    function get() public view returns(uint){
+        Numbers(number);
+        return number;
     }
 }
